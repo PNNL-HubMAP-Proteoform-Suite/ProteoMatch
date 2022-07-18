44f4f23626ef24ff874f2de1adb3b757ad6b6acf
David Degnan <40248272+David-Degnan@users.noreply.github.com>
2022-07-18 09:43:05 -0700

    Save test script

--- inst/testing.R
+++ inst/testing.R
@@ -13,12 +13,23 @@ run_proteomatch(
   ProteoformFile = "~/Desktop/HuBMAP_Data/Rat_Brain/Inputs/Rat_Brain_Proteins.csv",
   MSFile = "~/Desktop/HuBMAP_Data/Rat_Brain/Inputs/PeakData.csv",
   SettingsFile = "~/Desktop/HuBMAP_Data/Rat_Brain/Inputs/RatBrain_Defaults.xlsx",
-  Path = "~/Desktop/HuBMAP_Data/Rat_Brain/RatBrain_Results/"
+  Path = "~/Desktop/HuBMAP_Data/Results/RatBrain/"
 )
 
 ## Soybean
 
 # WT
-
+run_proteomatch(
+  ProteoformFile = "~/Desktop/HuBMAP_Data/Soybean_Example/ProteoMatch_Inputs/LFQ_soybean_topdown_new.csv",
+  MSFile = "~/Desktop/HuBMAP_Data/Soybean_Example/ProteoMatch_Inputs/MALDI_nodule_WT_DHA_recrystalhistoneprep_15um_IMAGE-avg11567_from100to200min.mzML",
+  SettingsFile = "~/Desktop/HuBMAP_Data/Soybean_Example/ProteoMatch_Inputs/Soybean_WT_Defaults.xlsx",
+  Path = "~/Desktop/HuBMAP_Data/Results/SoyBean/WT/"
+)
 
 # MU
+run_proteomatch(
+  ProteoformFile = "~/Desktop/HuBMAP_Data/Soybean_Example/ProteoMatch_Inputs/LFQ_soybean_topdown_new.csv",
+  MSFile = "~/Desktop/HuBMAP_Data/Soybean_Example/ProteoMatch_Inputs/MALDI_nodule_MUTANT_DHA_recrystalhistoneprep_15um_IMAGE-avg11567_from100to200min.mzML",
+  SettingsFile = "~/Desktop/HuBMAP_Data/Soybean_Example/ProteoMatch_Inputs/Soybean_MU_Defaults.xlsx",
+  Path = "~/Desktop/HuBMAP_Data/Results/SoyBean/MU/"
+)
